03675515ea60de3bba9cb5367b6842d7d636f5f3
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-06 04:07:12 +0000

    >  compile NEMU 3018209248 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  04:07:12 up 19:54,  1 user,  load average: 0.50, 0.27, 0.15 bda01eb776557d6edd2efff3cec67bfd85035b43

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -38,6 +38,9 @@ static int cmd_q(char *args) {
 
 static int cmd_help(char *args);
 static int cmd_si(char *args);
+static int cmd_info(char *args);
+
+
 static struct {
 	char *name;
 	char *description;
@@ -47,11 +50,25 @@ static struct {
 	{ "c", "Continue the execution of the program", cmd_c },
 	{ "q", "Exit NEMU", cmd_q },
 	{ "si","Single instruction", cmd_si},
+	{ "info","information",cmd_info},
 	/* TODO: Add more commands */
 
 };
 
 #define NR_CMD (sizeof(cmd_table) / sizeof(cmd_table[0]))
+static int cmd_info(char *args){
+	char *arg = strtok(NULL," ");
+	int i;
+	if(arg[0]=='r'){
+		for(i=R_EAX;i<=R_EDI;i++){
+			printf("%s\t0x%08x\n",regsl[i],reg_l(i));
+		}
+		printf("eip\t0x%08x\n",cpu.eip);
+	}else{
+		assert(0);
+	}
+	return 0;
+}
 static int cmd_si(char *args){
 	int n=1;
 	if(args != NULL){
