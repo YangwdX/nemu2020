c22e802b919a98ee6c3d3603b782e79b6159af32
tracer-ics2016 <tracer@njuics.org>
2020-09-19 09:23:02 +0000

    >  compile NEMU 3018209248 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  09:23:02 up 5 days,  7:03,  1 user,  load average: 0.63, 0.70, 0.59 3f8738c6e8f4e02cf3d32e4c853c8f0894a92dd

--- nemu/src/cpu/exec/call/call-template.h
+++ nemu/src/cpu/exec/call/call-template.h
@@ -2,16 +2,20 @@
 
 #define instr call
 
-/*static void do_execute(){
+static void do_execute(){
 	//push(eip)
 	//read operand address
 	//eip += relative address
-	DATA_TYPE_S
+	reg_l(R_ESP) -= DATA_BYTE;
+	swaddr_write(reg_l(R_ESP), 4 , cpu.eip + DATA_BYTE);
+	DATA_TYPE_S displacement = op_src->val;
+	print_asm("call %x",cpu.eip + 1 + displacement + displacement);
+	cpu.eip += displacement;
 }
 
 make_instr_helper(i);
-*/
-make_helper (concat(call_i_,SUFFIX)){
+
+/*make_helper (concat(call_i_,SUFFIX)){
 	int len = concat(decode_i_,SUFFIX)(eip + 1);
 	reg_l(R_ESP) -= DATA_BYTE;
 	swaddr_write(reg_l(R_ESP), 4 , cpu.eip + len);
@@ -19,5 +23,5 @@ make_helper (concat(call_i_,SUFFIX)){
 	print_asm("call %x",cpu.eip + 1 + len + displacement);
 	cpu.eip += displacement;
 	return len + 1;
-}
+}*/
 #include "cpu/exec/template-end.h"
