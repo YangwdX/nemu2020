3364e28370485abc524f09c407360f5368cef406
tracer-ics2016 <tracer@njuics.org>
2020-09-27 15:20:19 +0000

    >  compile NEMU 3018209248 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:20:19 up  2:32,  1 user,  load average: 0.97, 1.11, 1.06 d1425215de08cb3b73adecbedc7a24364b912b63

--- nemu/src/cpu/exec/string/rep.c
+++ nemu/src/cpu/exec/string/rep.c
@@ -53,7 +53,10 @@ make_helper(repnz) {
 			  );
 
 		/* TODO: Jump out of the while loop if necessary. */
-
+		if((ops_decoded.opcode == 0xa6
+				|| ops_decoded.opcode == 0xa7	
+				|| ops_decoded.opcode == 0xae	
+				|| ops_decoded.opcode == 0xaf) && cpu.ZF == 1) break;
 	}
 
 #ifdef DEBUG
