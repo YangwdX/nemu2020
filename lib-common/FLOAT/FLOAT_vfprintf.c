e11757f786b9e4491dc993a24ff7417945a8a200
tracer-ics2016 <tracer@njuics.org>
2020-10-03 11:55:05 +0000

    >  run 3018209248 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:55:05 up 5 days, 23:06,  1 user,  load average: 0.52, 0.24, 0.24 27c47e5f1efc00ac75c70f5e63cd9f4b976e8ec

--- lib-common/FLOAT/FLOAT_vfprintf.c
+++ lib-common/FLOAT/FLOAT_vfprintf.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 #include <stdint.h>
 #include "FLOAT.h"
-#include <sys/mman.h>
 
 extern char _vfprintf_internal;
 extern char _fpmaxtostr;
-extern char _ppfs_setargs;
 extern int __stdio_fwrite(char *buf, int len, FILE *stream);
 
 __attribute__((used)) static int format_FLOAT(FILE *stream, FLOAT f) {
@@ -18,19 +16,7 @@ __attribute__((used)) static int format_FLOAT(FILE *stream, FLOAT f) {
 	 */
 
 	char buf[80];
-	int sign = f & 0x80000000, len, cd = 0;
-	if(sign) f = (~f) + 1;
-	int hehe = 500000000, i = 15;
-	for(i = 15; i >= 0; i--) {
-		if(f & (1 << i)) cd += hehe;
-		hehe >>= 1;
-	}
-	while(cd > 999999) cd /= 10;
-	if(sign) {
-		len = (sprintf) (buf, "-%d.%06d", ((int)(f) >> 16), cd);
-	} else {
-		len = (sprintf) (buf, "%d.%06d", ((int)(f) >> 16), cd);
-	}
+	int len = sprintf(buf, "0x%08x", f);
 	return __stdio_fwrite(buf, len, stream);
 }
 
@@ -40,28 +26,7 @@ static void modify_vfprintf() {
 	 * is the code section in _vfprintf_internal() relative to the
 	 * hijack.
 	 */
-	int addr = &_vfprintf_internal;
-	char *sub = (char *)(addr + 0x306 - 0xb);
-	*sub = 0x8;
-	sub = (char *)(addr + 0x306 - 0xa);
-	*sub = 0xff;	//guess what?
-	sub = (char *)(addr + 0x306 - 0x9);
-	*sub = 0x32;	//guess what?
-	sub = (char *)(addr + 0x306 - 0x8);
-	*sub = 0x90;	//guess what?
 
-	sub = (char *)(addr + 0x306 - 30);
-	*sub = 0x90;
-	sub = (char *)(addr + 0x306 - 29);
-	*sub = 0x90;
-	sub = (char *)(addr + 0x306 - 33);
-	*sub = 0x90;
-	sub = (char *)(addr + 0x306 - 34);
-	*sub = 0x90; 
-
-	int *pos = (int *)(addr + 0x307);
-
-	*pos += (int)format_FLOAT - (int)(&_fpmaxtostr);
 #if 0
 	else if (ppfs->conv_num <= CONV_A) {  /* floating point */
 		ssize_t nf;
@@ -102,15 +67,6 @@ static void modify_vfprintf() {
 }
 
 static void modify_ppfs_setargs() {
-	int addr = &_ppfs_setargs;
-
-
-	char *pos = (char *)(addr + 0x71);
-	*pos = 0xeb;
-	pos = (char *)(addr + 0x72);
-	*pos = 0x30;
-	pos = (char *)(addr + 0x73);
-	*pos = 0x90;
 	/* TODO: Implement this function to modify the action of preparing
 	 * "%f" arguments for _vfprintf_internal() in _ppfs_setargs().
 	 * Below is the code section in _vfprintf_internal() relative to
