61242c977b57f57f3b866fcec61a678c9dc9006f
tracer-ics2016 <tracer@njuics.org>
2020-10-14 12:59:55 +0000

    >  compile NEMU 10602121 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:59:55 up 1 day, 10:22,  1 user,  load average: 1.35, 0.98, 0.72 b4fa3ca5e3be05bc4d110f70b699e4fbd7ef2dfa

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,8 +1,16 @@
 #include "common.h"
-
+#include <stdlib.h>
+#include "burst.h"
+#include "cpu/reg.h"
+#define BLOCK_SIZE 64
+#define STORAGE_SIZE_L1 64*1024
+#define STORAGE_SIZE_L2 4*1024*1024
+#define EIGHT_WAY 8
+#define SIXTEEN_WAY 16
 uint32_t dram_read(hwaddr_t, size_t);
 void dram_write(hwaddr_t, size_t, uint32_t);
 
+CPU_state cpu;
 /* Memory accessing interfaces */
 
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
