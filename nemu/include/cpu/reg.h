7b31a0e78ecca08610831017d46fa21ae59c7e86
tracer-ics2016 <tracer@njuics.org>
2020-10-16 02:44:56 +0000

    >  compile NEMU 3018209248 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  02:44:56 up 3 days, 7 min,  1 user,  load average: 2.04, 1.94, 1.68 14daa0387fd15d32acb62c21f6b545f12799b50d

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -6,6 +6,7 @@
 enum { R_EAX, R_ECX, R_EDX, R_EBX, R_ESP, R_EBP, R_ESI, R_EDI };
 enum { R_AX, R_CX, R_DX, R_BX, R_SP, R_BP, R_SI, R_DI };
 enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
+enum { R_ES, R_CS, R_SS, R_DS, R_FS, R_GS };
 
 /* TODO: Re-organize the `CPU_state' structure to match the register
  * encoding scheme in i386 instruction format. For example, if we
@@ -97,9 +98,11 @@ static inline int check_reg_index(int index) {
 #define reg_l(index) (cpu.gpr[check_reg_index(index)]._32)
 #define reg_w(index) (cpu.gpr[check_reg_index(index)]._16)
 #define reg_b(index) (cpu.gpr[check_reg_index(index) & 0x3]._8[index >> 2])
+#define reg_s(index) (cpu.sr[check_sreg_index(index)])
 
 extern const char* regsl[];
 extern const char* regsw[];
 extern const char* regsb[];
+extern const char* regss[];
 
 #endif
