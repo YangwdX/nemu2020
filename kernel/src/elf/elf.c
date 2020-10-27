266a868fe31100c21297f265cf2be9194092036a
tracer-ics2016 <tracer@njuics.org>
2020-10-27 12:50:39 +0000

    >  compile kernel 3018209248 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:50:39 up 2 days,  1:13,  1 user,  load average: 1.55, 1.10, 0.96 e619311fede37b7efb51b6ca1a33a667743dfc84

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -18,8 +18,8 @@ uint32_t get_ucr3();
 
 uint32_t loader() {
 	Elf32_Ehdr *elf;
-	Elf32_Phdr *ph = NULL, *eph;
-
+	//Elf32_Phdr *ph = NULL, *eph;
+	Elf32_Phdr *ph = NULL;
 	uint8_t buf[4096];
 
 #ifdef HAS_DEVICE
@@ -37,22 +37,25 @@ uint32_t loader() {
 
 	/* Load each program segment */
 	//panic("please implement me");
-	for(ph = (void *)buf + elf->e_phoff, eph = ph + elf->e_phnum; ph < eph; ph ++) {
+	int i = 0;
+	ph = (void *)(buf + elf->e_phoff);
+	//for(ph = (void *)buf + elf->e_phoff, eph = ph + elf->e_phnum; ph < eph; ph ++) {
+	for(; i < elf->e_phnum; i++, ph++) {
 		/* Scan the program header table, load each segment into memory */
 		if(ph->p_type == PT_LOAD) {
 
-                        uint32_t pa = ph->p_vaddr;
-
+                        //uint32_t pa = ph->p_vaddr;
+			ph->p_vaddr = mm_malloc(ph->p_vaddr, ph->p_memsz);
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
-			ramdisk_read((void *)pa, ELF_OFFSET_IN_DISK + ph->p_offset, ph->p_filesz); 
-			 
+			//ramdisk_read((void *)pa, ELF_OFFSET_IN_DISK + ph->p_offset, ph->p_filesz); 
+			 ramdisk_read((void *)ph->p_vaddr, ph->p_offset, ph->p_filesz);
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
-                        memset((void *)pa + ph->p_filesz, 0, ph->p_memsz - ph->p_filesz);
-
+                        //memset((void *)pa + ph->p_filesz, 0, ph->p_memsz - ph->p_filesz);
+			memset((void *)(ph->p_vaddr + ph->p_filesz), 0, ph->p_memsz - ph->p_filesz);
 #ifdef IA32_PAGE
 			/* Record the program break for future use. */
 			extern uint32_t cur_brk, max_brk;
