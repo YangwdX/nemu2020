39f7a98a723591b0b8d52fc5a1170de5ea5405ea
tracer-ics2016 <tracer@njuics.org>
2020-10-28 08:52:52 +0000

    >  compile NEMU 3018209248 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  08:52:52 up 2 days, 21:15,  1 user,  load average: 1.33, 1.28, 1.10 4991d9b3b66625aa37c06609466f39e0da5f8106

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -156,6 +156,8 @@ static int cmd_q(char *args) {
 	return -1;
 }
 
+static int cmd_page(char *args);
+
 static int cmd_help(char *args);
 
 static struct {
@@ -174,12 +176,22 @@ static struct {
         { "p", "Evaluate the value of expression", cmd_p },
 	{ "w", "Set watchpoint", cmd_w },
 	{ "d", "Delete watchpoint", cmd_d },
-	{ "bt", "Display backtrace", cmd_bt }
-
+	{ "bt", "Display backtrace", cmd_bt },
+	{ "page", "Print page addr translation result", cmd_page}
 };
 
 #define NR_CMD (sizeof(cmd_table) / sizeof(cmd_table[0]))
-
+static int cmd_page(char *args){
+	if(args == NULL) return 0;
+	lnaddr_t lnaddr;
+	sscanf(args, "%x", &lnaddr);
+	hwaddr_t hwaddr = page_translate(lnaddr, 1);
+	if(!(cpu.cr0.protect_enable && cpu.cr0.paging)) {
+		printf("Page Addr Transform Fail!\n");
+	}
+	else printf("Page-trans Result: 0x%x -> 0x%x\n", lnaddr, hwaddr);
+	return 0;
+}
 static int cmd_help(char *args) {
 	/* extract the first argument */
 	char *arg = strtok(NULL, " ");
